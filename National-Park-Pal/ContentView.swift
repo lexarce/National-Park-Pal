646d23e4d200bc603c83ebdf9cf602ca10bcf774
lexarce <lexarce7@gmail.com>
2025-03-20 15:43:56 -0700

    Initial Commit

---
+++ National-Park-Pal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  National-Park-Pal
+//
+//  Created by Alexis Arce on 3/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ National-Park-Pal/National_Park_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  National_Park_PalApp.swift
+//  National-Park-Pal
+//
+//  Created by Alexis Arce on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct National_Park_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
