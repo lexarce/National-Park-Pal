5b5c0c9f2cadfae5db50676003c980446b65c35e
lexarce <lexarce7@gmail.com>
2025-03-20 15:46:04 -0700

    Initial commit

--- National-Park-Pal/ContentView.swift
+++ National-Park-Pal/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("National Park Pal")
         }
         .padding()
     }
